d8c4bae8f3f8b8dd02549aa291309ff1b78d0018
oveldman <oveldman@gmail.com>
2025-12-01 13:05:42 +0000

    Integrate project definitions and update Aspire host initialization

--- Sources/MadWorldNL.Umiko.Aspire/AppHost.cs
+++ Sources/MadWorldNL.Umiko.Aspire/AppHost.cs
@@ -1,3 +1,16 @@
+using MadWorldNL.Umiko;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var api = builder.AddProject<Projects.Api>(ProjectDefinitions.Api)
+    .WithUrlForEndpoint("http", c => c.DisplayText = "ApiInsecure")
+    .WithUrlForEndpoint("https", c => c.DisplayText = "ApiSecure")
+    .WithExternalHttpEndpoints();
+
+builder.AddProject<Projects.Web>(ProjectDefinitions.Web)
+    .WithUrlForEndpoint("http", c => c.DisplayText = "WebInsecure")
+    .WithUrlForEndpoint("https", c => c.DisplayText = "WebSecure")
+    .WithExternalHttpEndpoints()
+    .WaitFor(api);
 
 builder.Build().Run();
---
+++ Sources/MadWorldNL.Umiko.Aspire/ProjectDefinitions.cs
@@ -0,0 +1,7 @@
+namespace MadWorldNL.Umiko;
+
+public static class ProjectDefinitions
+{
+    public const string Api = nameof(Api);
+    public const string Web = nameof(Web);
+}
