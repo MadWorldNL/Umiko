a088c0b252dc59cf2270fe38a716a051f28815ef
oveldman <github@oscarveldman.eu>
2026-01-25 13:07:25 +0100

    Add API project with OpenAPI support and integrate into solution structure

--- source/MadWorldNL.Umiko.Controllers.Web.Administrators/Program.cs
+++ source/MadWorldNL.Umiko.Controllers.Web.Administrators/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MadWorldNL.Umiko.Controllers.Web.Administrators;
+using MadWorldNL.Umiko.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
