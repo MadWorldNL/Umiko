8d4e814a104a01e244807c99a37a461fb6ae5099
oveldman <oveldman@gmail.com>
2026-01-27 12:59:26 +0100

    Integrate OpenTelemetry: Add support for metrics and tracing with OTLP export. Update dependencies and configuration, including `OpenTelemetry.Extensions.Hosting`.

---
+++ source/MadWorldNL.Umiko.Controllers.Api/Configurations/OpenTelemetryExtensions.cs
@@ -0,0 +1,47 @@
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+namespace MadWorldNL.Umiko.Configurations;
+
+public static class OpenTelemetryExtensions
+{
+    public static WebApplicationBuilder AddOpenTelemetry(this WebApplicationBuilder builder)
+    {
+        // Setup logging to be exported via OpenTelemetry
+        builder.Logging.AddOpenTelemetry(logging =>
+        {
+            logging.IncludeFormattedMessage = true;
+            logging.IncludeScopes = true;
+        });
+
+        var otel = builder.Services.AddOpenTelemetry();
+
+        // Add Metrics for ASP.NET Core and our custom metrics and export via OTLP
+        otel.WithMetrics(metrics =>
+        {
+            // Metrics provider from OpenTelemetry
+            metrics.AddAspNetCoreInstrumentation();
+            
+            // Metrics provides by ASP.NET Core in .NET 8
+            metrics.AddMeter("Microsoft.AspNetCore.Hosting");
+            metrics.AddMeter("Microsoft.AspNetCore.Server.Kestrel");
+        });
+
+        // Add Tracing for ASP.NET Core and our custom ActivitySource and export via OTLP
+        otel.WithTracing(tracing =>
+        {
+            tracing.AddAspNetCoreInstrumentation();
+            tracing.AddHttpClientInstrumentation();
+        });
+
+        // Export OpenTelemetry data via OTLP, using env vars for the configuration
+        var OtlpEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+        if (OtlpEndpoint != null)
+        {
+            otel.UseOtlpExporter();
+        }
+        
+        return builder;
+    }
+}
--- source/MadWorldNL.Umiko.Controllers.Api/Program.cs
+++ source/MadWorldNL.Umiko.Controllers.Api/Program.cs
@@ -1,4 +1,8 @@
+using MadWorldNL.Umiko.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddOpenTelemetry();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
