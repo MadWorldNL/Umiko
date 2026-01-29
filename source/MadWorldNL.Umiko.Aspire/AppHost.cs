30248485cfb87e27006f17d47fe373f6bd4abd84
oveldman <github@oscarveldman.eu>
2026-01-29 16:47:55 +0100

    Add Bus project with OpenAPI support and integrate PostgreSQL and RabbitMQ resources

--- source/MadWorldNL.Umiko.Aspire/AppHost.cs
+++ source/MadWorldNL.Umiko.Aspire/AppHost.cs
@@ -2,10 +2,45 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Api>("Api");
+var postgres = builder.AddPostgres("Postgres");
+var postgresDb = postgres.AddDatabase("PostgresDb");
 
-builder.AddProject<Web_Administrators>("Web-Administrators");
+var rabbitmq = CreateMessagingResource();
 
-builder.AddProject<Web_Users>("Web-Users");
+var api = builder.AddProject<Api>("Api")
+    .WaitFor(postgresDb)
+    .WaitFor(rabbitmq)
+    .WithReference(postgresDb)
+    .WithReference(rabbitmq);
+
+var bus = builder.AddProject<Bus>("Bus")
+    .WaitFor(postgresDb)
+    .WaitFor(rabbitmq)
+    .WithReference(postgresDb)
+    .WithReference(rabbitmq);
+
+builder.AddProject<Web_Administrators>("Web-Administrators")
+    .WaitFor(api)
+    .WithReference(api)
+    .WaitFor(bus)
+    .WithReference(bus);
+
+builder.AddProject<Web_Users>("Web-Users")
+    .WaitFor(api)
+    .WithReference(api)
+    .WaitFor(bus)
+    .WithReference(bus);
 
 builder.Build().Run();
+
+return;
+
+IResourceBuilder<RabbitMQServerResource> CreateMessagingResource()
+{
+    var username = builder.AddParameter("Messaging-Username", secret: true);
+    var password = builder.AddParameter("Messaging-Password", secret: true);
+
+    return builder.AddRabbitMQ("Messaging", username, password)
+        .WithDataVolume(isReadOnly: false)
+        .WithManagementPlugin();
+}
