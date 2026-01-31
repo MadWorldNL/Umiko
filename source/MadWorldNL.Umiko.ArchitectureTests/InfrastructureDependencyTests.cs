d66a21ea3aef11966a341097a13b73126431721f
oveldman <github@oscarveldman.eu>
2026-01-31 12:12:46 +0100

    Add architecture tests for ensuring Infrastructure layer dependency rules

---
+++ source/MadWorldNL.Umiko.ArchitectureTests/InfrastructureDependencyTests.cs
@@ -0,0 +1,34 @@
+using ArchUnitNET.Fluent;
+using ArchUnitNET.xUnit;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace MadWorldNL.Umiko;
+
+public class InfrastructureDependencyTests : BaseArchitectureTests
+{
+    [Fact]
+    public void Postgresql_ShouldNotDependOnFunctionsOrControllers()
+    {
+        IArchRule rule = Types().That().Are(PostgresqlLayer).Should()
+            .NotDependOnAny(FunctionsLayer)
+            .AndShould().NotDependOnAny(ApiLayer)
+            .AndShould().NotDependOnAny(BusLayer)
+            .AndShould().NotDependOnAny(WebAdministratorsLayer)
+            .AndShould().NotDependOnAny(WebUsersLayer);
+
+        rule.Check(Architecture);
+    }
+
+    [Fact]
+    public void RabbitMQ_ShouldNotDependOnFunctionsOrControllers()
+    {
+        IArchRule rule = Types().That().Are(RabbitMqLayer).Should()
+            .NotDependOnAny(FunctionsLayer)
+            .AndShould().NotDependOnAny(ApiLayer)
+            .AndShould().NotDependOnAny(BusLayer)
+            .AndShould().NotDependOnAny(WebAdministratorsLayer)
+            .AndShould().NotDependOnAny(WebUsersLayer);
+
+        rule.Check(Architecture);
+    }
+}
