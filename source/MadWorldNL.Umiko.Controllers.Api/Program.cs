170ac7f99dd62cb7bce15d609d93c53124cbe529
oveldman <github@oscarveldman.eu>
2026-02-03 18:52:47 +0100

    Add `Scalar.AspNetCore` package and map scalar API endpoints for Api and Bus services in development mode

--- source/MadWorldNL.Umiko.Controllers.Api/Program.cs
+++ source/MadWorldNL.Umiko.Controllers.Api/Program.cs
@@ -1,5 +1,6 @@
 using MadWorldNL.Umiko.Configurations;
 using MadWorldNL.Umiko.Endpoints;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
