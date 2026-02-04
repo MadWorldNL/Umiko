a1e4223c5dddb7174910c28faf24a5d00325d98a
oveldman <github@oscarveldman.eu>
2026-02-04 19:01:11 +0100

    Add Aspire test projects, integration tests, and health check configurations

---
+++ source/MadWorldNL.Umiko.Aspire.Tests/AppHost.cs
@@ -0,0 +1,41 @@
+using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var postgresDb = builder
+    .AddPostgres("Postgres")
+    .AddDatabase("PostgresDb");
+
+var rabbitmq = builder.AddRabbitMQ("Messaging");
+
+var api = builder.AddProject<Api>("Api")
+    .WaitFor(postgresDb)
+    .WaitFor(rabbitmq)
+    .WithReference(postgresDb)
+    .WithReference(rabbitmq)
+    .WithHttpHealthCheck("/health");
+
+var bus = builder.AddProject<Bus>("Bus")
+    .WaitFor(postgresDb)
+    .WaitFor(rabbitmq)
+    .WithReference(postgresDb)
+    .WithReference(rabbitmq)
+    .WithHttpHealthCheck("/health");
+
+builder.AddProject<Web_Administrators>("Web-Administrators")
+    .WithExternalHttpEndpoints()
+    .WaitFor(api)
+    .WaitFor(bus)
+    .WithReference(api)
+    .WithReference(bus)
+    .WithHttpHealthCheck("/health.txt");
+
+builder.AddProject<Web_Users>("Web-Users")
+    .WithExternalHttpEndpoints()
+    .WaitFor(api)
+    .WaitFor(bus)
+    .WithReference(api)
+    .WithReference(bus)
+    .WithHttpHealthCheck("/health.txt");
+
+builder.Build().Run();
