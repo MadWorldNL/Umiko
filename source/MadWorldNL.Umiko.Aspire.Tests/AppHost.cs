74cf92e851da6ec16bae71d0d8e73d1ea24fad1b
oveldman <oveldman@gmail.com>
2026-02-04 21:37:17 +0100

    Simplify AppHost and build workflow by removing `latest` Docker image tags and unnecessary image pulls.

--- source/MadWorldNL.Umiko.Aspire.Tests/AppHost.cs
+++ source/MadWorldNL.Umiko.Aspire.Tests/AppHost.cs
@@ -4,15 +4,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var postgresDb = builder
     .AddPostgres("Postgres")
-    .WithImageTag("latest")
     .AddDatabase("PostgresDb");
 
-var rabbitmq = builder.AddRabbitMQ("Messaging")
-    .WithImageTag("latest");
+var rabbitmq = builder.AddRabbitMQ("Messaging");
 
 var api = builder.AddProject<Api>("Api")
-    .WaitFor(postgresDb)
-    .WaitFor(rabbitmq)
     .WithReference(postgresDb)
     .WithReference(rabbitmq)
     .WithHttpHealthCheck("/health");
