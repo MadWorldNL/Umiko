fcca468564a2e46ffa7dd79bcdcd9c4426262b02
oveldman <oveldman@gmail.com>
2026-02-04 21:37:59 +0100

    Simplify AppHost by removing unused PostgreSQL and RabbitMQ references.

--- source/MadWorldNL.Umiko.Aspire.Tests/AppHost.cs
+++ source/MadWorldNL.Umiko.Aspire.Tests/AppHost.cs
@@ -2,15 +2,7 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgresDb = builder
-    .AddPostgres("Postgres")
-    .AddDatabase("PostgresDb");
-
-var rabbitmq = builder.AddRabbitMQ("Messaging");
-
 var api = builder.AddProject<Api>("Api")
-    .WithReference(postgresDb)
-    .WithReference(rabbitmq)
     .WithHttpHealthCheck("/health");
 
 builder.Build().Run();
